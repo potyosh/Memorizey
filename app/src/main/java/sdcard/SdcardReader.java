a9726a91ed3314467516ce5bad463b188305a635
yoshi <yoshi@watanabeyoshiyuki-no-MacBook-Pro.local>
2015-01-10 15:08:02 +0900

    分離したSdcardReaderクラスから読み込み処理をおこなう

--- app/src/main/java/sdcard/SdcardReader.java
+++ app/src/main/java/sdcard/SdcardReader.java
@@ -1,38 +1,27 @@
-package yoshi.memorizey;
+package sdcard;
 
 import android.os.Environment;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 
-import sdcard.SdcardReader;
+/**
+ * Created by yoshi on 2015/01/10.
+ */
+public class SdcardReader {
 
-
-public class MainActivity extends ActionBarActivity {
-
-    //設定値
-    private static final String DEFAULT_ENCORDING = "UTF-8";  //デフォルトのエンコード
-    private static final int DEFAULT_READ_LENGTH = 8192;      //一度に読み込むバッファサイズ
-    private String fileName = "/sdcard/temp/sample.txt";
+    private static final String DEFAULT_ENCORDING = "UTF-8";
+    private static final int DEFAULT_READ_LENGTH = 8192;
 
     //ストリームから読み込み、バイト配列で返す
     public static final byte[] readStream(InputStream inputStream, int readLength) throws IOException {
+
         final ByteArrayOutputStream byteStream = new ByteArrayOutputStream();    //一時バッファのように使う
         final byte[] bytes = new byte[readLength];    //read() 毎に読み込むバッファ
         final BufferedInputStream bis = new BufferedInputStream(inputStream, readLength);
@@ -98,71 +87,5 @@ public class MainActivity extends ActionBarActivity {
         File file = new File(fileName);
         InputStream is = new FileInputStream(absPath);
         return loadText(is, DEFAULT_ENCORDING);
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button button = (Button) findViewById(R.id.yoshiButton);
-
-        //"mnt/sdcard/sample.txt" or "sdcard/sample.txt" 等になる(端末による)
-        //String fileName = "/sdcard/temp/sample.txt";
-        String text = null;
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "aaa",
-                        Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        TextView myText = (TextView) findViewById(R.id.textView2);
-        myText.setText("hyooooooooo");
-
-        try {
-            text = SdcardReader.loadTextSDCard(fileName);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //Log.d("tag", text); でも良い
-        System.out.println("hogegegegeg"+fileName);
-
-        File file = new File("storage/emulated/0/", "aaa.txt");
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file), 100);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
