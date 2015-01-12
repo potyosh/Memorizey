da72e3dcc613081c45251c1dcf1326caf9fc02ad
yoshi <yoshi@watanabeyoshiyuki-no-MacBook-Pro.local>
2015-01-12 11:15:39 +0900

    ソースコード整理。不要な関数やインポート等削除

--- app/src/main/java/yoshi/memorizey/MainActivity.java
+++ app/src/main/java/yoshi/memorizey/MainActivity.java
@@ -1,8 +1,8 @@
 package yoshi.memorizey;
 
-import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -10,95 +10,14 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.UnsupportedEncodingException;
 
 import sdcard.SdcardReader;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    //設定値
-    private static final String DEFAULT_ENCORDING = "UTF-8";  //デフォルトのエンコード
-    private static final int DEFAULT_READ_LENGTH = 8192;      //一度に読み込むバッファサイズ
     private String fileName = "/sdcard/temp/sample.txt";
-
-    //ストリームから読み込み、バイト配列で返す
-    public static final byte[] readStream(InputStream inputStream, int readLength) throws IOException {
-        final ByteArrayOutputStream byteStream = new ByteArrayOutputStream();    //一時バッファのように使う
-        final byte[] bytes = new byte[readLength];    //read() 毎に読み込むバッファ
-        final BufferedInputStream bis = new BufferedInputStream(inputStream, readLength);
-
-        try {
-            int len = 0;
-            while ((len = bis.read(bytes, 0, readLength)) > 0) {
-                byteStream.write(bytes, 0, len);    //ストリームバッファに溜め込む
-            }
-            return byteStream.toByteArray();    //byte[] に変換
-
-        } finally {
-            try {
-                byteStream.reset();     //すべてのデータを破棄
-                bis.close();            //ストリームを閉じる
-            } catch (Exception e) {
-                //IOException
-            }
-        }
-    }
-
-    //ストリームから読み込み、テキストエンコードして返す
-    public static final String loadText(InputStream inputStream, String charsetName)
-            throws IOException, UnsupportedEncodingException {
-        return new String(readStream(inputStream, DEFAULT_READ_LENGTH), charsetName);
-    }
-
-    //ローカルシステムから、テキストファイルを読み込む
-    public static final String loadTextLocal(String fileName) throws IOException, FileNotFoundException {
-        InputStream is = new FileInputStream(fileName);
-        return loadText(is, DEFAULT_ENCORDING);
-    }
-
-    // SDCard のマウント状態をチェックする(Android 用)
-    public static final boolean isMountSDCard() {
-        String state = Environment.getExternalStorageState();
-        if (state.equals(Environment.MEDIA_MOUNTED)) {
-            return true;   //マウントされている
-        } else {
-            return false;  //マウントされていない
-        }
-    }
-
-    // SDCard のルートディレクトリを取得(Android 用)
-    public static final File getSDCardDir() {
-        return Environment.getExternalStorageDirectory();
-    }
-
-    // SDCard 内の絶対パスに変換(Android 用)
-    public static final String toSDCardAbsolutePath(String fileName) {
-        //return getSDCardDir().getAbsolutePath() + File.separator + fileName;
-        return getSDCardDir() + File.separator + fileName;
-
-    }
-
-    // SDCard から、テキストファイルを読み込む(Android 用)
-    public static final String loadTextSDCard(String fileName) throws IOException {
-        if (!isMountSDCard()) {  //マウント状態のチェック
-            throw new IOException("No Mount");
-        }
-        String absPath = toSDCardAbsolutePath("sample.txt");  //SDCard 内の絶対パスに変換
-        //InputStream is = new FileInputStream(absPath);
-        File file = new File(fileName);
-        InputStream is = new FileInputStream(absPath);
-        return loadText(is, DEFAULT_ENCORDING);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -106,9 +25,6 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         Button button = (Button) findViewById(R.id.yoshiButton);
-
-        //"mnt/sdcard/sample.txt" or "sdcard/sample.txt" 等になる(端末による)
-        //String fileName = "/sdcard/temp/sample.txt";
         String text = null;
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -127,20 +43,9 @@ public class MainActivity extends ActionBarActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        //Log.d("tag", text); でも良い
-        System.out.println("hogegegegeg"+fileName);
+        Log.d("tag", text);
+        //System.out.println("hogegegegeg"+fileName);
 
-        File file = new File("storage/emulated/0/", "aaa.txt");
-        try {
-            file.createNewFile();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        try {
-            BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(file), 100);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
     }
 
 
